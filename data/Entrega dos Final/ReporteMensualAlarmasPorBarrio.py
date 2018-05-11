150ddb53cec0cea6fd0a77dbc93652eeda15912c
Diego Fernando Riveros Lancheros <df.riveros11@sis.virtual.uniandes.edu.co>
2018-05-11 01:44:17 -0500

    Up

--- data/Entrega dos Final/ReporteMensualAlarmasPorBarrio.py
+++ data/Entrega dos Final/ReporteMensualAlarmasPorBarrio.py
@@ -1,10 +1,21 @@
 import pymongo as pym
+from bson.objectid import ObjectId
+from pymongo import MongoClient
 
-from pym import MongoClient
 client = MongoClient('localhost', 27017)
-db = clienteMongo['apirestfulencincominutos']
-collection = db.Alertas
+db = client['apirestfulencincominutos']
+alertas = db.alertas
 
-cursor = collection.find({'Correo': (correo)})
-        for document in cursor:
-            rol = document['Rol']
+print(alertas.find_one({'_id': ObjectId("5af4f5304e7b7d312ca09199")}))
+
+
+
+# The web framework gets post_id from the URL and passes it as a string
+def get(post_id):
+    # Convert from string to ObjectId:
+    document = client.db.collection.find_one({'_id': ObjectId(post_id)})
+
+
+#cursor = collection.find({'Correo': (correo)})
+#        for document in cursor:
+#            rol = document['Rol']
